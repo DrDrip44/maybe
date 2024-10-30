7f54cd4460b06c2532d63d82bee8c190c9f15342
DTharp1s <DTharp1s@capetigers.com>
2024-10-30 09:41:23 -0500

    one

---
+++ javascript/lht_table.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Dalton Tharp
+   Date:   10/28/24
+
+	
+*/
+
+// more varibles
+console.log(thisDay);
+console.log(eventDate);
+console.log(eventTime);
+
+/* current date */
+var thisDay = new Date("August 30, 2018");
+
+
+// possible problems
+var tableHTML = <table id='eventTable'></table>;
+
+// try to create the table, if you can't ask for help
+
+// other varibles for the table
+var endDate = new Date(thisDay.getTime() + 14*24*60*60*1000);
+
+
+// loop that loops through the length of the eventDates array. use i as a counter var
+for ( var i = 0; i < eventDates; i++) {
+   
+}
+   
